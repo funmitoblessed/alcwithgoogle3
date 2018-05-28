040a9975e5f487e21ed2026a5629daa04cca2fce
Funmito <funmito.odefemi@outlook.com>
2018-05-28 17:51:46 +0000

    JavaScript Code

--- L12 - ShirtSize/script.js
+++ L12 - ShirtSize/script.js
@@ -16,8 +16,24 @@ else statements, create a series of logical expressions that logs the size of a 
 */
 
 // change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
-var shirtWidth = 23;
-var shirtLength = 30;
-var shirtSleeve = 8.71;
+var shirtWidth = 18;
+var shirtLength = 29;
+var shirtSleeve = 8.47;
 
 // Write your if/else code here
+
+if ((shirtWidth >= 28 && shirtWidth < 30) && (shirtLength >= 34 && shirtLength < 35) && (shirtSleeve >= 10.13 && shirtSleeve < 10.38)) {
+    console.log("3XL");
+} else if ((shirtWidth >= 26 && shirtWidth < 28) && (shirtLength >= 33 && shirtLength < 34) && (shirtSleeve >= 9.63 && shirtSleeve < 10.13)) {
+    console.log("2XL");
+} else if ((shirtWidth >= 24 && shirtWidth < 26) && (shirtLength >= 31 && shirtLength < 33) && (shirtSleeve >= 8.88 && shirtSleeve < 9.63)) {
+    console.log("XL");
+} else if ((shirtWidth >= 22 && shirtWidth < 24) && (shirtLength >= 30 && shirtLength < 31) && (shirtSleeve >= 8.63 && shirtSleeve < 8.88)) {
+    console.log("L");
+} else if ((shirtWidth >= 20 && shirtWidth < 22) && (shirtLength >= 29 && shirtLength < 30) && (shirtSleeve >= 8.38 && shirtSleeve < 8.63)) {
+    console.log("M");
+} else if ((shirtWidth >= 18 && shirtWidth < 20) && (shirtLength >= 28 && shirtLength < 29) && (shirtSleeve >= 8.13 && shirtSleeve < 8.38)) {
+    console.log("S");
+} else {
+    console.log("N/A");
+}
