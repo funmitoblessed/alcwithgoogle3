aad491f1e88238a24f24c08106806454d43031de
Funmito <funmito.odefemi@outlook.com>
2018-05-28 16:05:04 +0000

    Lesson 12 Project 3-7

---
+++ L12 - ShirtSize/script.js
@@ -0,0 +1,23 @@
+/*
+*
+Programming Quiz: What do I Wear ? (3 - 7) *
+        *
+        Using
+if /
+else statements, create a series of logical expressions that logs the size of a t - shirt based on the measurements of:
+    *
+    -shirtWidth *
+    -shirtLength *
+    -shirtSleeve *
+    *
+    Use the chart above to print out one of the following correct values:
+    *
+    -S, M, L, XL, 2 XL, or 3 XL 
+*/
+
+// change the values of `shirtWidth`, `shirtLength`, and `shirtSleeve` to test your code
+var shirtWidth = 23;
+var shirtLength = 30;
+var shirtSleeve = 8.71;
+
+// Write your if/else code here
