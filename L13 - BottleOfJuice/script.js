989bf7be8f70f1a7fe302d3e410c6488fcdeaf26
Funmito <funmito.odefemi@outlook.com>
2018-05-31 15:53:54 +0000

    99 Bottles of Juice Quiz

---
+++ L13 - BottleOfJuice/script.js
@@ -0,0 +1,27 @@
+/*
+ * Programming Quiz: 99 Bottles of Juice (4-2)
+ *
+ * Use the following `while` loop to write out the song "99 bottles of juice".
+ * Log the your lyrics to the console.
+ *
+ * Note
+ *   - Each line of the lyrics needs to be logged to the same line.
+ *   - The pluralization of the word "bottle" changes from "2 bottles" to "1 bottle" to "0 bottles".
+ */
+
+var num = 99;
+
+while (num > 0) {
+    // check value of num
+    if ((num > 2) && (num <= 99)) {
+        // print lyrics using num
+        console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num - 1) + " bottles of juice on the wall!");
+    } else if (num === 2) {
+        console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num - 1) + " bottle of juice on the wall!");
+    } else if (num === 1) {
+        // don't forget to check pluralization on the last line!
+        console.log(num + " bottle of juice on the wall! " + num + " bottle of juice! Take one down, pass it around... " + (num - 1) + " bottles of juice on the wall!");
+    }
+    // decrement num
+    num = num - 1;
+}
