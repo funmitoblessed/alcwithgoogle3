95d4d2d7bfe43f5c1fc2dc84d678ed6058698093
Funmito <funmito.odefemi@outlook.com>
2018-06-01 13:52:08 +0000

    Countdown, Lift Off! Quiz Challenge (4-3)

--- L13.6 - Countdown, Liftoff!/script.js
+++ L13.6 - Countdown, Liftoff!/script.js
@@ -5,3 +5,48 @@
  */
 
 // your code goes here
+
+var T = 60;
+
+while (T >= 0) {
+    if (T > 50 && T <= 60) {
+        console.log("T-" + T + " seconds");
+    }
+    if (T === 50) {
+        console.log("Orbiter transfers from ground to internal power");
+    }
+    if (T > 31 && T < 50) {
+        console.log("T-" + T + " seconds");
+    }
+    if (T === 31) {
+        console.log("Ground launch sequencer is go for auto sequence start");
+    }
+    if (T > 16 && T < 31) {
+        console.log("T-" + T + " seconds");
+    }
+    if (T === 16) {
+        console.log("Activate launch pad sound suppression system")
+    }
+    if (T > 10 && T < 16) {
+        console.log("T-" + T + " seconds");
+    }
+    if (T === 10) {
+        console.log("Activate main engine hydrogen burnoff system");
+    }
+    if (T > 6 && T < 10) {
+        console.log("T-" + T + " seconds");
+    }
+    if (T === 6) {
+        console.log("Main engine start")
+    }
+    if (T > 1 && T < 6) {
+        console.log("T-" + T + " seconds");
+    }
+    if (T === 1) {
+        console.log("T-" + T + " seconds");
+    }
+    if (T === 0) {
+        console.log("Solid rocket booster ignition and liftoff!");
+    }
+    T = T - 1;
+}
