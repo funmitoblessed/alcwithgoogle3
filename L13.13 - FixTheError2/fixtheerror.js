49b1a02d3a697ea0cb708795a93293a5c89454b9
Funmito <funmito.odefemi@outlook.com>
2018-06-01 14:51:33 +0000

    Lesson 13 Quizzes

---
+++ L13.13 - FixTheError2/fixtheerror.js
@@ -0,0 +1,18 @@
+/*
+Directions:
+The for loop below has an error. Fix it!
+
+for (var k = 0 k < 200 k++) {
+  console.log(k);
+}
+*/
+
+/*
+ * Programming Quiz: Fix the Error 2 (4-6)
+ */
+
+// fix the for loop
+// my solution
+for (var k = 0; k < 200; k++) {
+    console.log(k);
+}
