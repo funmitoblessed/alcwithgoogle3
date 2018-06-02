71bb60d780383c6d77e720bd624e511d292d4144
ofunmito <funmito.odefemi@outlook.com>
2018-06-02 09:00:51 +0100

    Introduction to Functions

---
+++ L14.5_6 - LaughItOff/laugh_1.js
@@ -0,0 +1,20 @@
+/*
+
+Directions:
+
+Declare a function called laugh() that returns "hahahahahahahahahaha!". 
+Print the value returned from the laugh() function to the console.
+
+*/
+
+/*
+ * Programming Quiz: Laugh it Off 1 (5-1)
+ */
+
+// your code goes here
+function laugh() {
+    var mylaugh = "hahahahahahahahahaha!"
+    return mylaugh
+}
+// call the laugh function
+console.log(laugh());
---
+++ L14.5_6 - LaughItOff/laugh_2.js
@@ -0,0 +1,33 @@
+/*
+
+Directions:
+Write a function called laugh() that takes one parameter, num that represents the number of "ha"s to return.
+
+TIP: You might need a loop to solve this!
+
+Here's an example of the output and how to call the function that you will write:
+
+console.log(laugh(3));
+Prints: "hahaha!"
+
+*/
+
+/*
+ * Programming Quiz: Laugh it Off 2 (5-2)
+ *
+ * Write a function called `laugh` with a parameter named `num` that represents the number of "ha"s to return.
+ *
+ * Note:
+ *  - make sure your the final character is an exclamation mark ("!")
+ *  - make sure that your function produces the correct results when it is called multiple times
+ */
+
+function laugh(num) {
+    var output = '';
+    for (var i = 1; i <= num; i++) {
+        output += 'ha'
+    }
+    output += '!'
+    return output;
+}
+console.log(laugh(5));
