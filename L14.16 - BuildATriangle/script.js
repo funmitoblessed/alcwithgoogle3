b3e420c5db20c3fd2380260aca149a08a3acb9f7
Funmito <funmito.odefemi@outlook.com>
2018-06-03 21:23:15 +0000

    buildTriangle improved

--- L14.16 - BuildATriangle/script.js
+++ L14.16 - BuildATriangle/script.js
@@ -3,6 +3,7 @@
  */
 
 // creates a line of * for a given length
+
 function makeLine(length) {
     var line = "";
     for (var j = 1; j <= length; j++) {
@@ -10,25 +11,15 @@ function makeLine(length) {
     }
     return line + "\n";
 }
+
 // your code goes here.  Make sure you call makeLine() in your own code.
 
 function buildTriangle(length) {
-    for (var i = 1; i <= length; i++) {
+    for (var i = 1; i < length; i++) {
         console.log(makeLine(i));
     }
+    return makeLine(i);
 }
-buildTriangle(10);
 
 // test your code by uncommenting the following line
-// console.log(buildTriangle(10));
-
-
-// find a way to automate
-/*
-function buildTriangle(length) {
-	triangle = makeLine(length-9) + makeLine(length-8) + makeLine(length-7) + makeLine(length-6) + makeLine(length-5) + makeLine(length-4) + makeLine(length-3) + makeLine(length-2) + makeLine(length-1) + makeLine(length);
-    //return triangle;
-    console.log(triangle);
-}	
-buildTriangle(10)
-*/
+console.log(buildTriangle(10));
