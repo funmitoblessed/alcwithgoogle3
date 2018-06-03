27eb2b81cf4a1926918ba12ed56af6e4cf66ed5e
Funmito <funmito.odefemi@outlook.com>
2018-06-03 20:56:41 +0000

    Lesson 14 Quiz

---
+++ L14.20 - Laugh/laugh.js
@@ -0,0 +1,19 @@
+/*
+Directions:
+Write an anonymous function expression that stores a function in a variable called "laugh"
+and outputs the number of "ha"s that you pass in as an argument.
+
+laugh(3);
+
+Returns: hahaha!
+*/
+
+// SOLUTION
+
+/*
+ * Programming Quiz: Laugh (5-4)
+ */
+
+var laugh = /* finish the function expression */
+
+console.log(laugh(10));
