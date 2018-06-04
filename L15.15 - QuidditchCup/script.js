889206f21a508a092b1ba76720e8c00f997f0d5f
Funmito <funmito.odefemi@outlook.com>
2018-06-04 17:49:51 +0000

    Quidditich

--- L15.15 - QuidditchCup/script.js
+++ L15.15 - QuidditchCup/script.js
@@ -24,6 +24,12 @@ Create a function called hasEnoughPlayers() that takes the team array as an argu
 
 var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
 
-
+function hasEnoughPlayers(team) {
+    if (team.length >= 7) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 console.log(hasEnoughPlayers(team));
