4f6df7bddc3c08df7f14ba4a36614c7de42bca8f
Funmito <funmito.odefemi@outlook.com>
2018-06-06 16:19:15 +0000

    Array Quizzes

--- L15.20 - AnotherLoop/forEach.js
+++ L15.20 - AnotherLoop/forEach.js
@@ -26,3 +26,11 @@ var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
 ];
 
 // Write your code here
+
+test.forEach(function(test, index, array) {
+    if (test % 3 === 0) {
+        test += 100;
+        array[index] = test;
+    }
+});
+console.log(test);
