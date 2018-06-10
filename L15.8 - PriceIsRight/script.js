e713f7447aeb0ac9977d785c84511fcdb9827fd8
Funmito <funmito.odefemi@outlook.com>
2018-06-10 09:47:21 +0000

    comments

--- L15.8 - PriceIsRight/script.js
+++ L15.8 - PriceIsRight/script.js
@@ -2,6 +2,7 @@
  * Lesson 15-8 Quiz: The Price is Right
  *
  * Directions:
+ * 
  * Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
  * var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
  * TIP: The 1st element of any array has an index of 0.
