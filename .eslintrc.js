8f5bd39fc1985363fbce108590e51757289e7700
Funmito <funmito.odefemi@outlook.com>
2018-06-10 18:27:40 +0000

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,3 @@
 module.exports = {
-    "env": {
-        "browser": true
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            "space"
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+    "extends": "google"
 };
