1062043a494873fcd190a7bd412e800c1d172efa
ofunmito <funmito.odefemi@outlook.com>
2018-06-12 15:13:09 +0100

    jQuery toggleClass Quiz

---
+++ L19.2 - toggleClass/app.js
@@ -0,0 +1,31 @@
+/*
+ * Lesson 19.2 Quiz: you.toggleClass
+ * For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
+*/
+
+// MY SOLUTION
+// don't change this variable!
+var featuredArticle;
+
+featuredArticle = $( "li.article-item" ).toggleClass( "featured" ); // your code starts here!
+
+
+// Cameron's  Solution
+var featured;
+
+featured = $('.featured');
+
+featured.toggleClass('featured');
+
+/*
+ * Lesson 19.3 Quiz: toggling Classes
+ * 
+ * For this quiz, remove the class 'featured' from Article #2 and add it to Article #3!
+ * 
+ * You must use jQuery's toggleClass method!
+*/
+
+// don't change these variable!
+var article2, article3;
+
+// your code goes here!
