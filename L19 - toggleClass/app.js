46a54631448b85c0ef59db61082b5c8d67398ed7
ofunmito <funmito.odefemi@outlook.com>
2018-06-14 16:48:16 +0100

    Quiz

--- L19 - toggleClass/app.js
+++ L19 - toggleClass/app.js
@@ -1,12 +1,12 @@
 /*
  * Lesson 19.2 Quiz: you.toggleClass
  * For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
-*/
+ */
 // don't change this variable!
-var featuredArticle;  
+var featuredArticle;
 // your code starts here!
 // MY SOLUTION
-featuredArticle = $( "li.article-item" ).toggleClass( "featured" ); // selects the 'list' item with class 'article-item' and toggles the class 'featured'
+featuredArticle = $("li.article-item").toggleClass("featured"); // selects the 'list' item with class 'article-item' and toggles the class 'featured'
 // Cameron's  Solution
 var featured;
 featured = $('.featured');
@@ -17,9 +17,9 @@ featured.toggleClass('featured');
  * Lesson 19.3 Quiz: toggling Classes
  * For this quiz, remove the class 'featured' from Article #2 and add it to Article #3!
  * You must use jQuery's toggleClass method!
-*/
+ */
 // don't change these variable!
-var article2, article3; 
+var article2, article3;
 // your code goes here!
 // MY SOLUTION
 article2 = $('.featured').toggleClass('featured'); // finds the element with the 'featured' class and toggles the class
@@ -35,9 +35,9 @@ article3.toggleClass('featured');
  * Lesson 19.4 Quiz: Changing Attributes
  * For this quiz, set the href of the <a> in the first nav item to "#1".
  * You must use jQuery's attr() method!
-*/
+ */
 // Start with this variable!
 var navList;
 // your code goes here!
 // MY SOLUTION
-navList = ;
+navList = $('ul.nav-list').first();
